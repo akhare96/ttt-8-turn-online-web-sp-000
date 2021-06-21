52aa85633b645e42ae9f037ba6511f128979e907
Ambar Khare <ambar.khare24@gmail.com>
2021-06-21 15:50:34 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,14 +40,6 @@ def position_taken?(board, index)
   end
 end
 
-turn method
-
-ask for input
-get input
-convert input to index
-if index is valid
-  make the move for index
-  show the board
-else
-  ask for input again until you get a valid input
+def turn(board)
+  puts "Please enter 1-9:"
 end
